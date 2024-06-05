5f9d0ebf008f11e507c43d3b177477862b3515fc
kondurusai <kondurusai863@gmail.com>
2024-06-05 13:06:19 +0530

    Xcountries completed

--- xcountries/src/component/country.js
+++ xcountries/src/component/country.js
@@ -1,7 +1,61 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+const CountryCard = ({ countryname, imageSrc, imageAlt }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "150px",
+        width: "150px",
+        border: "1px solid #eee",
+        margin: ".5rem",
+        padding: "1rem",
+      }}
+    >
+      <img
+        src={imageSrc}
+        alt={imageAlt}
+        style={{ width: "100px", height: "100px" }}
+      />
+      <p>{countryname}</p>
+    </div>
+  );
+};
 const Country = () => {
-  return <div>hellow countries</div>;
+  const [apidata, setApidata] = useState([]);
+  const API_CALL = "https://restcountries.com/v3.1/all";
+  let res = async () => {
+    try {
+      let response = await fetch(API_CALL);
+      let data = response.json();
+      data.then((res) => setApidata(res));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    res();
+  }, []);
+  console.log(apidata);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      {apidata.map((data, ind) => (
+        <CountryCard
+          countryname={data.name.common}
+          imageSrc={data.flags.png}
+          imageAlt={data.flags.alt}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Country;
